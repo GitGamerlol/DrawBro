fed5d483ac1238890149963b1bda9482d901335b
GitGamerlol <159203649+GitGamerlol@users.noreply.github.com>
2026-07-30 20:49:51 -0700

    Delete DrawBro/core directory

--- DrawBro/core/frame.py
+++
@@ -1,5 +0,0 @@
-from .layer import Layer
-
-class Frame:
-    def __init__(self,w=1024,h=1024):
-        self.layers=[Layer("Layer 1",w,h)]
--- DrawBro/core/project.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass
-class Project:
-    name:str="Untitled"
-    width:int=1024
-    height:int=1024
-    fps:int=12
-    current_frame:int=0
